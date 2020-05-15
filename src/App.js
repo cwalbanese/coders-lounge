65437a8a7f33864fbdac367203afb231cd4aa7fd
cwalbanese <631gti@gmail.com>
2020-05-15 17:26:26 -0400

    updated login auth to parent component

--- src/App.js
+++ src/App.js
@@ -10,12 +10,22 @@ import CreatePost from './components/CreatePost/CreatePost.js';
 import LoginMessage from './components/LoginMessage/LoginMessage';
 import Login from './components/Login/Login.js';
 import Footer from './components/Footer/Footer.js';
+import { setInStorage, getFromStorage } from './utils/storage.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       posts: [],
+      isLoading: true,
+      token: '',
+      signUpError: '',
+      signInError: '',
+      signInUsername: '',
+      signInPassword: '',
+      signUpUsername: '',
+      signUpPassword: '',
+      username: 'anonymous',
     };
   }
 
@@ -23,6 +33,118 @@ class App extends React.Component {
     fetch('http://localhost:8082/api/posts')
       .then((response) => response.json())
       .then((data) => this.setState({ posts: data }));
+  }
+
+  onTextboxChangeSignInUsername(event) {
+    this.setState({
+      signInUsername: event.target.value,
+    });
+  }
+
+  onTextboxChangeSignInPassword(event) {
+    this.setState({
+      signInPassword: event.target.value,
+    });
+  }
+
+  onTextboxChangeSignUpUsername(event) {
+    this.setState({
+      signUpUsername: event.target.value,
+    });
+  }
+
+  onTextboxChangeSignUpPassword(event) {
+    this.setState({
+      signUpPassword: event.target.value,
+    });
+  }
+
+  onSignUp() {
+    const { signUpUsername, signUpPassword } = this.state;
+    this.setState({
+      isLoading: true,
+    });
+
+    fetch('http://localhost:8082/api/users/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: signUpUsername,
+        password: signUpPassword,
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log('json', json);
+        if (json.success) {
+          this.setState({
+            signUpError: json.message,
+            isLoading: false,
+            signUpUsername: '',
+            signUpPassword: '',
+          });
+        } else {
+          this.setState({
+            signUpError: json.message,
+            isLoading: false,
+          });
+        }
+      });
+  }
+
+  onSignIn() {
+    const { signInUsername, signInPassword } = this.state;
+    this.setState({
+      isLoading: true,
+    });
+
+    fetch('http://localhost:8082/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: signInUsername,
+        password: signInPassword,
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.success) {
+          setInStorage('coders-lounge', { token: json.token });
+          this.setState({ username: signInUsername });
+          this.setState({
+            signInError: json.message,
+            isLoading: false,
+            signInPassword: '',
+            signInUsername: '',
+            token: json.token,
+          });
+        } else {
+          this.setState({
+            signInError: json.message,
+            isLoading: false,
+          });
+        }
+      });
+  }
+
+  logout() {
+    const obj = getFromStorage('coders-lounge');
+    if (obj && obj.token) {
+      const { token } = obj;
+
+      fetch('http://localhost:8082/api/users/logout?token=' + token)
+        .then((res) => res.json())
+        .then(() => {
+          this.setState({
+            token: '',
+            username: 'anonymous',
+          });
+        });
+    }
   }
 
   render() {
@@ -34,9 +156,24 @@ class App extends React.Component {
         <PostsMessage />
         <Posts posts={this.state.posts} />
         <LoginMessage />
-        <Login />
+        <Login
+          onTextboxChangeSignInUsername={this.onTextboxChangeSignInUsername}
+          onTextboxChangeSignInPassword={this.onTextboxChangeSignInPassword}
+          onTextboxChangeSignUpUsername={this.onTextboxChangeSignUpUsername}
+          onTextboxChangeSignUpPassword={this.onTextboxChangeSignUpPassword}
+          token={this.state.token}
+          signInError={this.state.signInError}
+          signInUsername={this.state.signInUsername}
+          signInPassword={this.state.signInPassword}
+          signUpUsername={this.state.signUpUsername}
+          signUpPassword={this.state.signUpPassword}
+          signUpError={this.state.signUpError}
+          onSignUp={this.onSignUp}
+          onSignIn={this.onSignIn}
+          logout={this.logout}
+        />
         <CreateMessage />
-        <CreatePost />
+        <CreatePost username={this.state.username} />
         <Footer />
       </div>
     );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import { setInStorage, getFromStorage } from '../../utils/storage.js';
+import { getFromStorage } from '../../utils/storage.js';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: true,
-      token: '',
-      signUpError: '',
-      signInError: '',
-      signInUsername: '',
-      signInPassword: '',
-      signUpUsername: '',
-      signUpPassword: '',
-      username: 'anonymous',
+      token: props.token,
+      signInError: props.signInError,
+      signInUsername: props.signInUsername,
+      signInPassword: props.signInPassword,
+      signUpUsername: props.signInUsername,
+      signUpPassword: props.signUpPassword,
+      signUpError: props.signUpError,
+      onTextboxChangeSignInUsername: props.onTextboxChangeSignInUsername,
+      onTextboxChangeSignInPassword: props.onTextboxChangeSignInPassword,
+      onTextboxChangeSignUpUsername: props.onTextboxChangeSignUpUsername,
+      onTextboxChangeSignUpPassword: props.onTextboxChangeSignUpPassword,
+      onSignUp: props.onSignUp,
+      onSignIn: props.onSignIn,
+      logout: props.logout,
     };
+    this.state.onTextboxChangeSignInUsername = this.state.onTextboxChangeSignInUsername.bind(
+      this
+    );
+    this.state.onTextboxChangeSignInPassword = this.state.onTextboxChangeSignInPassword.bind(
+      this
+    );
+    this.state.onTextboxChangeSignUpUsername = this.state.onTextboxChangeSignUpUsername.bind(
+      this
+    );
+    this.state.onTextboxChangeSignUpPassword = this.state.onTextboxChangeSignUpPassword.bind(
+      this
+    );
 
-    this.onTextboxChangeSignInUsername = this.onTextboxChangeSignInUsername.bind(
-      this
-    );
-    this.onTextboxChangeSignInPassword = this.onTextboxChangeSignInPassword.bind(
-      this
-    );
-    this.onTextboxChangeSignUpUsername = this.onTextboxChangeSignUpUsername.bind(
-      this
-    );
-    this.onTextboxChangeSignUpPassword = this.onTextboxChangeSignUpPassword.bind(
-      this
-    );
-
-    this.onSignUp = this.onSignUp.bind(this);
-    this.onSignIn = this.onSignIn.bind(this);
-    this.logout = this.logout.bind(this);
+    this.state.onSignUp = this.state.onSignUp.bind(this);
+    this.state.onSignIn = this.state.onSignIn.bind(this);
+    this.state.logout = this.state.logout.bind(this);
   }
 
   componentDidMount() {
@@ -45,172 +49,49 @@ class Login extends React.Component {
     }
   }
 
-  onTextboxChangeSignInUsername(event) {
-    this.setState({
-      signInUsername: event.target.value,
-    });
-  }
-
-  onTextboxChangeSignInPassword(event) {
-    this.setState({
-      signInPassword: event.target.value,
-    });
-  }
-
-  onTextboxChangeSignUpUsername(event) {
-    this.setState({
-      signUpUsername: event.target.value,
-    });
-  }
-
-  onTextboxChangeSignUpPassword(event) {
-    this.setState({
-      signUpPassword: event.target.value,
-    });
-  }
-
-  onSignUp() {
-    const { signUpUsername, signUpPassword } = this.state;
-    this.setState({
-      isLoading: true,
-    });
-
-    fetch('http://localhost:8082/api/users/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: signUpUsername,
-        password: signUpPassword,
-      }),
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        console.log('json', json);
-        if (json.success) {
-          this.setState({
-            signUpError: json.message,
-            isLoading: false,
-            signUpUsername: '',
-            signUpPassword: '',
-          });
-        } else {
-          this.setState({
-            signUpError: json.message,
-            isLoading: false,
-          });
-        }
-      });
-  }
-
-  onSignIn() {
-    const { signInUsername, signInPassword } = this.state;
-    this.setState({
-      isLoading: true,
-    });
-
-    fetch('http://localhost:8082/api/users/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: signInUsername,
-        password: signInPassword,
-      }),
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        console.log('json', json);
-        if (json.success) {
-          setInStorage('coders-lounge', { token: json.token });
-          this.setState({ username: signInUsername });
-          this.setState({
-            signInError: json.message,
-            isLoading: false,
-            signInPassword: '',
-            signInUsername: '',
-            token: json.token,
-          });
-        } else {
-          this.setState({
-            signInError: json.message,
-            isLoading: false,
-          });
-        }
-      });
-  }
-
-  logout() {
-    const obj = getFromStorage('coders-lounge');
-    if (obj && obj.token) {
-      const { token } = obj;
-
-      fetch('http://localhost:8082/api/users/logout?token=' + token)
-        .then((res) => res.json())
-        .then(() => {
-          this.setState({
-            token: '',
-            username: 'anonymous',
-          });
-        });
-    }
-  }
-
   render() {
-    const {
-      token,
-      signInError,
-      signInUsername,
-      signInPassword,
-      signUpUsername,
-      signUpPassword,
-      signUpError,
-    } = this.state;
-
-    if (!token) {
+    if (!this.state.token) {
       return (
         <div className="white-background">
           <div>
-            {signInError ? <h2>{signInError}</h2> : null}
+            {this.state.signInError ? <h2>{this.state.signInError}</h2> : null}
             <h2>login</h2>
             <input
               type="username"
               placeholder="Username"
-              value={signInUsername}
-              onChange={this.onTextboxChangeSignInUsername}
+              value={this.state.signInUsername}
+              onChange={this.state.onTextboxChangeSignInUsername}
             />
             <br />
             <input
               type="password"
               placeholder="Password"
-              value={signInPassword}
-              onChange={this.onTextboxChangeSignInPassword}
+              value={this.state.signInPassword}
+              onChange={this.state.onTextboxChangeSignInPassword}
             />
             <br />
-            <button onClick={this.onSignIn}>Login</button>
+            <button onClick={this.state.onSignIn}>Login</button>
           </div>
           <br />
           <br />
           <div>
-            {signUpError ? <h2>{signUpError}</h2> : null}
+            {this.state.signUpError ? <h2>{this.state.signUpError}</h2> : null}
             <h2>signup</h2>
             <input
               type="username"
               placeholder="Username"
-              value={signUpUsername}
-              onChange={this.onTextboxChangeSignUpUsername}
+              value={this.state.signUpUsername}
+              onChange={this.state.onTextboxChangeSignUpUsername}
             />
             <br />
             <input
               type="password"
               placeholder="Password"
-              value={signUpPassword}
-              onChange={this.onTextboxChangeSignUpPassword}
+              value={this.state.signUpPassword}
+              onChange={this.state.onTextboxChangeSignUpPassword}
             />
             <br />
-            <button onClick={this.onSignUp}>Signup</button>
+            <button onClick={this.state.onSignUp}>Signup</button>
           </div>
         </div>
       );
@@ -218,7 +99,7 @@ class Login extends React.Component {
     return (
       <div className="white-background">
         <h2>logged in</h2>
-        <button onClick={this.logout}>Logout</button>
+        <button onClick={this.state.logout}>Logout</button>
       </div>
     );
   }
