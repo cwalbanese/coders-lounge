aee757bb24af5a8bac56887f460c983287bd4fa0
cwalbanese <631gti@gmail.com>
2020-05-17 15:56:32 -0400

    added date to post

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -7,9 +7,12 @@ function Posts(props) {
       <h2>posts</h2>
       {posts.reverse().map((result) => (
         <div key={result._id} className="results">
-          <p>{result.post}</p>
+          <p className="post-item">"{result.post}"</p>
           <p className="posted-by">
             posted by: <span>{result.username}</span>
+          </p>
+          <p className="posted-on">
+            posted on: <span>{result.time.slice(0, 10)}</span>
           </p>
         </div>
       ))}
